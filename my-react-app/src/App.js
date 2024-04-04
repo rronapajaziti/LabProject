2d3ffd1b8d678ee50367484e698ebfbb57a583a1
LaidaR3 <rusinovcilaida13@gmail.com>
2024-04-04 11:56:36 +0200

    Ndryshime

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
 
-function App() {
+import 'bootstrap/dist/css/bootstarap.min.css';
+
+import Sidebar from './Sidebar';
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Hello World</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Sidebar></Sidebar>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
