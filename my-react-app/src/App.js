b94d085107dae1c8fca1c03e3d01ed6677aa6727
LaidaR3 <rusinovcilaida13@gmail.com>
2024-04-06 14:15:34 +0200

    Sidebar - List - Icons

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 import Sidebar from "./Sidebar";
 
--- my-react-app/src/Sidebar.js
+++ my-react-app/src/Sidebar.js
@@ -1,26 +1,43 @@
 import React from 'react'
 
+
 function Sidebar(){
     return (
-        <div className='bg-white'>
-            <div>
-                <i className='bi bi-bootstarap-fill my-2'></i>
+        <div className='header'>
+            <div className='m-2'>
+                <i className='bi bi-bootstarap-fill me-2 fs-4'></i>
                 <span className='brand-name fs-4'>Readopia</span>
             </div>
             <hr className='text-dark'/>
-            <div className='list-group list-group-flush'>
-                <a className='list-group-item list-group-itemm-action my-2'>
-                    <i className='bi bi-spedometer2'></i>
+            <div className='list-group listgroup-flush'>
+                <a className='list-group-item py-2'>
+                    <i  id= 'dashboardname' className='bi bi-speedometer2 fs-5 me-2'></i>
                     <span>Dashboard</span>
                 </a>
-                <a className='list-group-item list-group-itemm-action my-2'>
-                    <i className='bi bi-house'></i>
+                <a className='list-group-item py-2'>
+                    <i className='bi bi-house fs-5 me-2'></i>
                     <span>Home</span>
+                </a>
+                <a className='list-group-item py-2'>
+                    <i class="bi bi-person-badge fs-5 me-2"></i>
+                    <span>Stafi</span>
+                </a>
+                <a className='list-group-item py-2'>
+                    <i className="bi bi-book fs-5 me-2"></i>
+                    <span>Libra</span>
+                </a>
+                <a className='list-group-item py-2'>
+                    <i class="bi bi-grid-1x2 fs-5 me-2"></i>
+                    <span>Kategorite</span>
+                </a>
+                <a className='list-group-item py-2'>
+                <i class="bi bi-bookmark-star-fill fs-5 me-2"></i>
+                    <span>Aksesoret</span>
                 </a>
             </div>
 
         </div>
     )
-}
+} 
 
 export default Sidebar
