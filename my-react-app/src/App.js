3084f6c12606a3b01ea063d319cef0d39d7959f5
LaidaR3 <rusinovcilaida13@gmail.com>
2024-04-06 16:51:15 +0200

    CSS - Sidebar

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import './style.css'
 
 
 import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div>
-      <Sidebar></Sidebar>
+    <div className='container-fluid  custom-bg min-vh-100'>
+      <div className='row '>
+        <div className='col-2 min-vh-100 sidebarColor-bg'>
+          <Sidebar />
+        </div>
+        <div className="col-auto">
+
+        </div>
+      </div>
+
     </div>
   );
 }
--- my-react-app/src/Sidebar.js
+++ my-react-app/src/Sidebar.js
@@ -1,41 +1,42 @@
 import React from 'react'
+import './style.css'
 
 
 function Sidebar(){
     return (
-        <div className='header'>
-            <div className='m-2'>
+        <div className='sidebar p-2' style={{ height: '100vh' }}>
+            <div className='me-2'>
                 <i className='bi bi-bootstarap-fill me-2 fs-4'></i>
                 <span className='brand-name fs-4'>Readopia</span>
             </div>
             <hr className='text-dark'/>
             <div className='list-group listgroup-flush'>
                 <a className='list-group-item py-2'>
-                    <i  id= 'dashboardname' className='bi bi-speedometer2 fs-5 me-2'></i>
-                    <span>Dashboard</span>
+                    <i  id= 'dashboardname' className='bi bi-speedometer2 fs-5 me-3'></i>
+                    <span className='fs-5'>Dashboard</span>
                 </a>
-                <a className='list-group-item py-2'>
-                    <i className='bi bi-house fs-5 me-2'></i>
+                <a className='list-group-item py-2 '>
+                    <i className='bi bi-house fs-5 me-3'></i>
                     <span>Home</span>
                 </a>
                 <a className='list-group-item py-2'>
-                    <i class="bi bi-person-badge fs-5 me-2"></i>
+                    <i class="bi bi-person-badge fs-5 me-3"></i>
                     <span>Stafi</span>
                 </a>
-                <a className='list-group-item py-2'>
-                    <i className="bi bi-book fs-5 me-2"></i>
+                <a className='list-group-item py-2 '>
+                    <i className="bi bi-book fs-5 me-3"></i>
                     <span>Libra</span>
                 </a>
-                <a className='list-group-item py-2'>
-                    <i class="bi bi-grid-1x2 fs-5 me-2"></i>
+                <a className='list-group-item py-2 '>
+                    <i class="bi bi-grid-1x2 fs-5 me-3"></i>
                     <span>Kategorite</span>
                 </a>
                 <a className='list-group-item py-2'>
-                    <i class="bi bi-vector-pen fs-5 me-2"></i>
+                    <i class="bi bi-vector-pen fs-5 me-3"></i>
                     <span>Autoret</span>
                 </a>
-                <a className='list-group-item py-2'>
-                <i class="bi bi-bookmark-star-fill fs-5 me-2"></i>
+                <a className='list-group-item py-2 '>
+                <i class="bi bi-bookmark-star-fill fs-5 me-3"></i>
                     <span>Aksesoret</span>
                 </a>
             </div>
