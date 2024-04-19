b314b4b71a2f11db90987c23300f5d9ea0fec765
DonikaNuredinii <dn67445@ubt-uni.net>
2024-04-19 18:23:00 +0200

    Route path per kategori te librave

---
+++ my-react-app/src/Components/AddCategories.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect, Fragment } from "react";
+import { Row, Col, Form, Button } from "react-bootstrap";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
