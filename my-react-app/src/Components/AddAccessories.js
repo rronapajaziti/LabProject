614d107d484fc5edf7201be04e8180d787d9c4e1
rronapajaziti <rrona2004@gmail.com>
2024-04-21 20:45:43 +0200

    Update AddAccessories.js
    ndryshimi per eleminimin  e errorave

--- my-react-app/src/Components/AddAccessories.js
+++ my-react-app/src/Components/AddAccessories.js
@@ -3,204 +3,212 @@ import { Row, Col, Form, Button } from "react-bootstrap";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Link } from "react-router-dom";
 
 const AddAccessories = () => {
-    const[AccessoriesID, setAccessoriesID] = useState("");
-    const [Image, setImage] = useState("");
-    const [Name, setName] = useState("");
-    const [Seller, setSeller] = useState("");
-    const [Description, setDescription] = useState("");
-    const [Dimensions, setDimensions] = useState("");
-    const [Price, setPrice] = useState("");
-    const [Quantity, setQuantity] = useState("");
-    const [success, setSuccess] = useState(false);
+  const [AccessoriesID, setAccessoriesID] = useState("");
+  const [Image, setImage] = useState("");
+  const [Name, setName] = useState("");
+  const [Seller, setSeller] = useState("");
+  const [Description, setDescription] = useState("");
+  const [Dimensions, setDimensions] = useState("");
+  const [Price, setPrice] = useState("");
+  const [Quantity, setQuantity] = useState("");
+  const [success, setSuccess] = useState(false);
 
-    const [data, setData] = useState([]);
-    useEffect(() => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios
+      .get(`https://localhost:7200/api/Accessories`)
+      .then((result) => {
+        setData(result.data);
+      })
+      .catch((error) => {
+        toast.error("Failed to get data: " + error.message);
+      });
+  };
+
+  const handleSave = () => {
+    const url = `https://localhost:7200/api/Accessories`;
+    const data = {
+      AccessoriesID: AccessoriesID,
+      Image: Image,
+      Name: Name,
+      Seller: Seller,
+      Description: Description,
+      Dimensions: Dimensions,
+      Price: Price,
+      Quantity: Quantity,
+    };
+    axios
+      .post(url, data)
+      .then((result) => {
         getData();
-    }, []);
-
-    const getData = () => {
-        axios
-        .get(`https://localhost:7200/api/Accessories`)
-        .then((result) => {
-            setData(result.data);
-        })
-        .catch((error) => {
-            toast.error("Failed to get data: " + error.message);
-        });
-    };
-
-    const handleSave = () => {
-        const url = `https://localhost:7200/api/Accessories`;
-        const data = {
-            AccessoriesID: AccessoriesID,
-            Image: Image,
-            Name: Name,
-            Seller: Seller,
-            Description: Description,
-            Dimensions: Dimensions,
-            Price: Price,
-            Quantity: Quantity,
-        };
-        axios
-        .post(url, data)
-        .then((result) => {
-            getData();
-            clear();
-            toast.success("Accessory has been added");
-            setSuccess(true);
-        })
-        .catch((error) => {
-            toast.error("Failed to add Accessory: " + error.message);
-        });
-    };
-    const clear = () => {
-        setAccessoriesID("");
-        setImage("");
-        setName("");
-        setSeller("");
-        setDescription("");
-        setDimensions("");
-        setPrice("");
-        setQuantity("");
-    };
-    const handleClear = () => {
         clear();
-    };
-    return (
-        <Form className="AccessoriesForm ">
-            <ToastContainer></ToastContainer>
-            <Row>
-                <Col>
-                    <Form.Group controlId="formAccessoriesID">
-                        <Form.Label>AccessoriesID</Form.Label>
-                        <Form.Control
-                        type="text"
-                        placeholder="Enter Accessories ID"
-                        name="AccessoriesID"
-                        value={AccessoriesID}
-                        onChange={(e) => setAccessoriesID(e.target.value)}
-                        />
-                    </Form.Group>
-                </Col>
-                <Col>
-                    <Form.Group controlId="formImage">
-                        <Form.Label>Image</Form.Label>
-                        <Form.Control
-                        type="text"
-                        placeholder="Enter image URL"
-                        name="image"
-                        value={image}
-                        onChange={(e) => setImage(e.target.value)}
-                        />
-                    </Form.Group>
-                </Col>
-            </Row>
+        toast.success("Accessory has been added");
+        setSuccess(true);
+      })
+      .catch((error) => {
+        toast.error("Failed to add Accessory: " + error.message);
+      });
+  };
+  const clear = () => {
+    setAccessoriesID("");
+    setImage("");
+    setName("");
+    setSeller("");
+    setDescription("");
+    setDimensions("");
+    setPrice("");
+    setQuantity("");
+  };
+  const handleClear = () => {
+    clear();
+  };
+  return (
+    <Form className="AccessoriesForm ">
+      <ToastContainer></ToastContainer>
+      <Row>
+        <Col>
+          <Form.Group controlId="formAccessoriesID">
+            <Form.Label>AccessoriesID</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Accessories ID"
+              name="AccessoriesID"
+              value={AccessoriesID}
+              onChange={(e) => setAccessoriesID(e.target.value)}
+            />
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group controlId="formImage">
+            <Form.Label>Image</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter image URL"
+              name="image"
+              value={Image}
+              onChange={(e) => setImage(e.target.value)}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
 
-            <Row>
-                <Col>
-                    <Form.Group controlId="formName">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control
-                        type="text"
-                        placeholder="Enter Name"
-                        name="Name"
-                        value={Name}
-                        onChange={(e) => setName(e.target.value)}
-                        />
-                    </Form.Group>
-                </Col>
+      <Row>
+        <Col>
+          <Form.Group controlId="formName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Name"
+              name="Name"
+              value={Name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+        </Col>
 
-                <Col>
-                    <Form.Group controlId="formSeller">
-                        <Form.Label>Seller</Form.Label>
-                        <Form.Control
-                        type="text"
-                        placeholder="Enter Seller"
-                        name="Seller"
-                        value={author}
-                        onChange={(e) => setSeller(e.target.value)}
-                        />
-                    </Form.Group>
-                </Col>
-            </Row>
+        <Col>
+          <Form.Group controlId="formSeller">
+            <Form.Label>Seller</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Seller"
+              name="Seller"
+              value={Seller}
+              onChange={(e) => setSeller(e.target.value)}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
 
-            <Row>
-                <Col>
-                    <Form.Group controlId="formDescription">
-                        <Form.Label>Description</Form.Label>
-                        <Form.Control
-                        as="textarea"
-                        rows={3}
-                        placeholder="Enter description"
-                        name="Description"
-                        value={Description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        />
-                    </Form.Group>
-                </Col>
+      <Row>
+        <Col>
+          <Form.Group controlId="formDescription">
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              placeholder="Enter description"
+              name="Description"
+              value={Description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </Form.Group>
+        </Col>
 
-                <Col>
-                    <Form.Group controlId="formDimensions">
-                        <Form.Label>Dimensions</Form.Label>
-                        <Form.Control
-                        type="text"
-                        placeholder="Enter Dimensions"
-                        name="Dimensions"
-                        value={Dimensions}
-                        onChange={(e) => setDimensions(e.target.value)}
-                      />
-                    </Form.Group>
-                </Col>
-            </Row>
+        <Col>
+          <Form.Group controlId="formDimensions">
+            <Form.Label>Dimensions</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Dimensions"
+              name="Dimensions"
+              value={Dimensions}
+              onChange={(e) => setDimensions(e.target.value)}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
 
-            <Row>
-                <Col>
-                    <Form.Group controlId="formPrice">
-                        <Form.Label>Price</Form.Label>
-                        <Form.Control
-                        type="text"
-                        placeholder="Enter Price"
-                        name="Price"
-                        value={Price}
-                        onChange={(e) => setPrice(e.target.value)}
-                        />
-                    </Form.Group>
-                </Col>
+      <Row>
+        <Col>
+          <Form.Group controlId="formPrice">
+            <Form.Label>Price</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Price"
+              name="Price"
+              value={Price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </Form.Group>
+        </Col>
 
-                <Col>
-                    <Form.Group controlId="formQuantity">
-                        <Form.Label>Quantity</Form.Label>
-                        <Form.Control
-                        type="text"
-                        placeholder="Enter Quantity"
-                        name="Quantity"
-                        value={Quantity}
-                        onChange={(e) => setQuantity(e.target.value)}
-                        />
-                    </Form.Group>
-                </Col>
-            </Row>
+        <Col>
+          <Form.Group controlId="formQuantity">
+            <Form.Label>Quantity</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Quantity"
+              name="Quantity"
+              value={Quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
 
-            <Row>
-                {" "}
-                <Col>
-                    <Link to="/Accessories">
-                        <Button variant="dark" className="btn-addAccessories" onClick={handleSave} >
-                            Add
-                        </Button>
-                    </Link>
-                </Col>
+      <Row>
+        {" "}
+        <Col>
+          <Link to="/Accessories">
+            <Button
+              variant="dark"
+              className="btn-addAccessories"
+              onClick={handleSave}
+            >
+              Add
+            </Button>
+          </Link>
+        </Col>
+        <Col>
+          <Button
+            variant="dark"
+            className="btn-addAccessories"
+            onClick={handleClear}
+          >
+            Clear
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
 
-                <Col>
-                    <Button variant="dark" className="btn-addAccessories" onClick={handleClear}>
-                        Clear
-                    </Button>
-                </Col>
-            </Row>
-        </Form>
-    );
-    };
-
-    export default AddAccessories;
+export default AddAccessories;
