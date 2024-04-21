36dd108736c4e73022fbd888d3eae7c35d590735
DielliHana <diellona.shefkiu@gmail.com>
2024-04-21 08:10:46 +0200

    Create AddAccessories

---
+++ my-react-app/src/Components/AddAccessories.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect, Fragment } from "react";
+import { Row, Col, Form, Button } from "react-bootstrap";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const AddAccessory = () => {
+    const[accessoryID, setAccessoryID] = useState("");
+}
