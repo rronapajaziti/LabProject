e8e2072c6a013c2a879ffdb1391583db4c92aba6
DielliHana <diellona.shefkiu@gmail.com>
2024-04-21 13:28:43 +0200

    Create Accessories + Update

---
+++ WebApplication1/Models/Accessories.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace WebApplication1.Models
+{
+    public class Accessories
+    {
+        public int AccessoryID { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Seller { get; set; }
+        public string Description { get; set; }
+        public int Dimensions { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}
